6962c43f3b7ac27077881ee02f0bb1f041e32f95
IntContinua <44573217+IntContinua@users.noreply.github.com>
2018-11-07 19:44:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head></head>
+  <head>
+    <title>Programas universitarios</title>
+    <link rel="stylesheet" type="text/css" href="estilos/estilo.css">
+  </head>
 <body>
+  <h1>Directorio programas universtarios Colombia</h1>
 <?php
 ?>
 </body>
