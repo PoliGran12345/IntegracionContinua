523b7e240e7aa153fc6689afd2f589d8388b3db1
IntContinua <44573217+IntContinua@users.noreply.github.com>
2018-11-13 09:23:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,27 +2,34 @@
 <html>
 <head>
     <title>Programas universitarios</title>
-    <link rel="stylesheet" type="text/css" href="estilos/estilo.css">
+	<link rel="stylesheet" type="text/css" href="estilos/estilo.css">
 </head>
 <body>
-<h1>Directorio programas universtarios Colombia</h1>
- <!--// FORMULARIO BÚSQUEDAS -->
-<form action="" method="post">
-	<label>Ingrese ciudad</label>
-	<p><input type="text" name="ciudad" value="" /><p> <!-- ¿ponemos un listado? -->
-	<label>Ingrese nivel</label>
-	<p><input type="text" name="nivel" value="" /><p>
-	<input type="submit" value="Buscar" />
-</form>
-<!--// FIN FORMULARIO BÚSQUEDAS -->
+<div class="contenedor_1">
+	<h1>Directorio programas universtarios Colombia</h1>
+	 <!--// FORMULARIO BÚSQUEDAS -->
+	<form action="" method="post">
+		<label>Ingrese ciudad</label>
+		<p><input type="text" name="ciudad" value="" /><p> <!--¿ponemos un listado? -->
+		<label>Ingrese nivel</label>
+		<p><select name="nivel">
+		  <option value="1">PREGRADO</option>
+		  <option value="2">POSGRADO</option>
+		</select><p>
+		<label>¿Qué le gustaría estudiar?</label>
+		<p><input type="text" name="programa" value="" placeholder="EJ: MEDICINA, INGENIERIA" /><p>
+		<input type="submit" value="Buscar" />
+	</form>
+	<!--// FIN FORMULARIO BÚSQUEDAS -->
 <?php 
 
-if(isset($_POST['ciudad']) ) {
-    //$ciudad = $_POST['ciudad'];	
-	include("includes/resultados.php");
-} else {}
+	if(isset($_POST['ciudad']) && isset($_POST['nivel'])  ) {
+		//$ciudad = $_POST['ciudad'];	
+		include("includes/resultados.php");
+	} else {}
 
 ?>
+</div>
 </body>
 </html>
 <!-- #################################### 
